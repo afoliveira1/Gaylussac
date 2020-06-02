c9a4876bce0a03351acd49f4ad946e02de2ff8ad
afoliveira1 <40571415+afoliveira1@users.noreply.github.com>
2020-06-02 12:18:48 -0300

    Add files via upload

---
+++ ProgramaMediaSimples.py
@@ -0,0 +1,18 @@
+a = float(input('Entre com o primeiro número: '))
+print(a)
+b = float(input('Entre com o segundo número: '))
+print(b)
+c = float(input('Entre com o terceiro número: '))
+print(c)
+d = float(input('Entre com o quarto número: '))
+print(d)
+e = float(input('Entre com o quinto e último número: '))
+print(e)
+
+#Agora vamos calcular o valor médio
+
+m = (a+b+c+d+e)/5.0
+
+#Agora vamos apresentar o resultado
+
+print('O valor médio dos números inseridos é: ',m)
